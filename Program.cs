30aaa856e6e81f518379b7c0b20dcf1f2995895e
_RobertoMtz <roberto.martinezmn@gmail.com>
2019-10-07 16:01:56 -0500

    This is an example with added Newtonsoft

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetCoreCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
